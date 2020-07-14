46106f2dc5e1a3bafdabe6cc667f5d881c321222
bartwr <mail@bartroorda.nl>
2020-07-15 00:49:04 +0200

    Rebrand header navigation to G2U20 style

--- assets-changes.sh
+++ assets-changes.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 
+# Get current dir
 CUR_DIR=$(pwd)
-#declare -a kmd_coins=(REVS SUPERNET DEX PANGEA JUMBLR BET CRYPTO HODL MSHARK BOTS MGW COQUI WLC KV CEAL MESH MNZ AXO ETOMIC BTCH PIZZA BEER NINJA OOT BNTN CHAIN PRLPAY DSEC GLXT EQL)
-source $CUR_DIR/kmd_coins.sh
 
-for i in "${kmd_coins[@]}"
-do
-sed -i 's/3 \* 1e8/1 \* 1e4/g' $CUR_DIR/$i-explorer/node_modules/insight-api-komodo/lib/blocks.js
-sed -i "s/\"TAZ\":\"KMD\"/\"TAZ\":\"$i\"/g" $CUR_DIR/$i-explorer/node_modules/insight-ui-komodo/public/js/main.min.js
-#sed -i "s/\"TAZ\":\"\$i\"/\"TAZ\":\"$i\"/g" $CUR_DIR/$i-explorer/node_modules/insight-ui-komodo/public/js/main.min.js
-done
+# Update ticker from KMD to G2U20
+sed -i "s/\"KMD\":\"\G2U20\"/\"KMD\":\"G2U20\"/g" $CUR_DIR/G2U20-explorer/node_modules/insight-ui-komodo/public/js/main.min.js
+
+# sed -i 's/3 \* 1e8/1 \* 1e4/g' $CUR_DIR/$i-explorer/node_modules/insight-api-komodo/lib/blocks.js
